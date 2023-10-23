286cd1bc3c2fa9e28322b7b500cc387c94c98a73
lenovalsha <lenovalsha@gmail.com>
2023-10-22 21:18:37 -0600

    Add project files.

---
+++ BlackJack/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace BlackJack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStay = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblHouseTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(758, 111);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnHit
+            // 
+            this.btnHit.Location = new System.Drawing.Point(758, 189);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(75, 23);
+            this.btnHit.TabIndex = 5;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // btnStay
+            // 
+            this.btnStay.Location = new System.Drawing.Point(758, 235);
+            this.btnStay.Name = "btnStay";
+            this.btnStay.Size = new System.Drawing.Size(75, 23);
+            this.btnStay.TabIndex = 6;
+            this.btnStay.Text = "Stay";
+            this.btnStay.UseVisualStyleBackColor = true;
+            this.btnStay.Click += new System.EventHandler(this.btnStay_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.Green;
+            this.lblTotal.Location = new System.Drawing.Point(357, 544);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(51, 24);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "Total";
+            // 
+            // lblHouseTotal
+            // 
+            this.lblHouseTotal.AutoSize = true;
+            this.lblHouseTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHouseTotal.ForeColor = System.Drawing.Color.Red;
+            this.lblHouseTotal.Location = new System.Drawing.Point(357, 154);
+            this.lblHouseTotal.Name = "lblHouseTotal";
+            this.lblHouseTotal.Size = new System.Drawing.Size(51, 24);
+            this.lblHouseTotal.TabIndex = 8;
+            this.lblHouseTotal.Text = "Total";
+            this.lblHouseTotal.Click += new System.EventHandler(this.lblHouseTotal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 603);
+            this.Controls.Add(this.lblHouseTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnStay);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStay;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblHouseTotal;
+    }
+}
+
---
+++ BlackJack/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJack
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int playerTotal;
+        int HouseTotal;
+        int currentPlayerLocationIndex = 0;
+        int num;
+        int hit;
+        List<Label> labels = new List<Label>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnStay.Enabled = false;
+            btnHit.Enabled = false;
+        }
+
+        public void btnPlay_Click(object sender, EventArgs e)
+        {
+             Play();
+             btnPlay.Enabled = false;
+            int house1 = rnd.Next(1, 11);
+
+            //get random number for our player
+            int num1 = rnd.Next(1,11);
+            int num2 = rnd.Next(1,11);
+            //display the random numbers
+            CreateLabel(100, 100, house1,false); //house's first number
+
+            CreateLabel(100, 200, num1,false);
+            CreateLabel(150, 200, num2,false);        
+            playerTotal = num1 + num2;
+            HouseTotal = house1;
+            lblHouseTotal.Text = HouseTotal.ToString();
+
+            //if the player number is not 21 then give the choice of hit or stay
+            GetPlayerTotal(playerTotal);
+
+        }
+
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            hit++;
+            num = rnd.Next(1, 11); //this is only getting hit once
+
+            for (int i = 0; i < 1; i++)
+            {
+                
+
+                    num = rnd.Next(1, 11);
+                    CreateLabel(150 + (hit * 50), 200, num, false);
+                    playerTotal += num;
+                    lblTotal.Text = playerTotal.ToString();
+                GetPlayerTotal(playerTotal);
+                
+            }
+
+        }
+        private void GetPlayerTotal(int total)
+        {
+            lblTotal.Text = total.ToString();
+           if(total >= 21)
+            {
+                btnHit.Enabled = false;
+                btnStay.Enabled = false;
+                if(total == 21)
+                {
+                    Stay();
+                }
+                else
+                {
+                    MessageBox.Show("Bust");
+                    btnPlay.Enabled = true;
+                    ClearPanel();
+                }
+            }
+           else
+            {
+                btnHit.Enabled = true;
+                btnStay.Enabled = true;
+            }
+        }
+
+        private void CreateLabel(int x, int y, int value, bool type)
+        {
+            Point point = new Point (x,y);
+            Font font = new Font("Consolas", 10);
+            Label newLabel = new Label();
+            currentPlayerLocationIndex = type == false? 0 : currentPlayerLocationIndex;
+            newLabel.Text = value.ToString();
+            newLabel.Font = font;
+            newLabel.Location = point; // Change the index to 0 to use the first set of points
+            newLabel.AutoSize = true;
+            this.Controls.Add(newLabel);
+            labels.Add(newLabel);
+            if(type == true) {
+
+                currentPlayerLocationIndex++;
+            }
+        }
+
+        private void btnStay_Click(object sender, EventArgs e)
+        {
+            Stay();
+            
+        }
+        private void Stay()
+        {
+            num = rnd.Next(1, 11); //this is only getting hit once
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (HouseTotal < 17)
+                {
+                    num = rnd.Next(1, 11);
+                    CreateLabel(150 + (i * 50), 100, num, false);
+                    HouseTotal += num;
+                    lblHouseTotal.Text = HouseTotal.ToString();
+                }
+            }
+
+            if(HouseTotal > playerTotal && HouseTotal <=21)
+            {
+                MessageBox.Show("house wins");
+            }
+            else 
+            {
+                MessageBox.Show("Player wins");
+            }
+            btnPlay.Enabled = true;
+        }
+        private void Play()
+        {
+            btnPlay.Enabled = true;
+           ClearPanel();
+            hit = 0;
+
+            
+        }
+        private void ClearPanel()
+        {
+            foreach(Control control in labels)
+            {
+                if(control is Label)
+                {
+                    this.Controls.Remove(control);
+                    control.Dispose();
+                }
+            }
+        }
+
+        private void lblHouseTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
